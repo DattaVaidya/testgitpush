40496baa55f265cbb6eed6b2307081bae4a2fa8a
DattaVaidya <dattavaidya1@gmail.com>
2022-09-11 08:16:07 +0530

    first code to git through push

---
+++ mongodb.py
@@ -0,0 +1,12 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://dattavaidya1:Datta117711@cluster0.rwdzoar.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+d={
+    "name":"datta",
+    "surname":"vaidya",
+    "email_id":"dattavaidya1@gmail.com"
+}
+db1=client['mongotest']
+coll=db1['test']
+coll.insert_one(d)
