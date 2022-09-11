14e847e1bdd7f2ad54d2048d03c3c1358bc3eea5
DattaVaidya <dattavaidya1@gmail.com>
2022-09-11 08:20:30 +0530

    first code to git through push

--- mongodb.py
+++ mongodb.py
@@ -2,7 +2,7 @@ import pymongo
 client = pymongo.MongoClient("mongodb+srv://dattavaidya1:Datta117711@cluster0.rwdzoar.mongodb.net/?retryWrites=true&w=majority")
 db = client.test
 print(db)
-d={
+d11={
     "name":"datta",
     "surname":"vaidya",
     "email_id":"dattavaidya1@gmail.com"
